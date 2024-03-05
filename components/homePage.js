407ccef2a8ec259989bfbe8b87d0d457f1700599
Walexhino <adewaleadebayo630@gmail.com>
2024-03-05 20:24:48 +0100

    corrected error

--- components/homePage.js
+++ components/homePage.js
@@ -4,10 +4,10 @@ export default function HomePage() {
 
   const [parameters, setParameters] = React.useState()
   const [walletAddress, setWalletAddress] = React.useState(false)
-  const [maticBalance, setMaticBalance] = React.useState()``
+  const [maticBalance, setMaticBalance] = React.useState()
   const [centBalance, setCentBalance] = React.useState()
   const [isConnected, setIsConnected] = React.useState()
-  const [messageFromFlutter, setMessageFromFlutter] = React.useState()
+  const [messageFromFlutter, setMessageFromFlutter] = React.useState('')
   const [messageHandler, setMessageHandler] = React.useState()
   const [messageToFlutter, setMessageToFlutter] = React.useState()
   const [message,setMessage]=React.useState()
@@ -40,10 +40,12 @@ function sendMessageToFlutter(message) {
   window.Flutter.postMessage();
 }
 
+function handleReceiveMessage(event) {
+  setMessageFromFlutter(event.data);
+}
+
 React.useEffect(() => {
-  function handleReceiveMessage(event) {
-    setMessageFromFlutter(event.data);
-  }
+  
 
   window.addEventListener('message', handleReceiveMessage);
 
@@ -92,15 +94,14 @@ React.useEffect(() => {
           <div className={`bg-[#3166FF] rounded-[15px] pt-2 pb-1 px-3 text-[#FFFFFF] text-md font-octarinebold cursor-pointer`}>
             submit
           </div>
-        </button> 
-
-        
-      <div className="text-md">Matic tx: {messageFromFlutter}</div> {/* Display message here */}
+        </button>
+        <div>Message from Flutter: {messageFromFlutter.text}</div>
+        {/* <div className="text-md">Matic tx :{messageFromFlutter} </div> */}
+           
     </div>
 
-        <div className="text-md">Matic tx : </div>
-      
-         
+       
+    
 
     </>
   )
