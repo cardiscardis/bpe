3482d970af467179a49bf636a4dbe82749f75cc8
Walexhino <adewaleadebayo630@gmail.com>
2024-03-07 22:56:31 +0100

    test matics

--- components/homePage.js
+++ components/homePage.js
@@ -93,6 +93,8 @@ export default function HomePage() {
         </button>
         <div className="text-md">Token tx : </div>
       </div>
+
+
       <div className="flex-col justify-center align-center pt-3">
         <input
           id="matic"
@@ -114,11 +116,43 @@ export default function HomePage() {
           </div>
         </button>
 
+        
+
     {/* <div>Message from Flutter: {messageFromFlutter.text}</div>
     <div className="text-md">Matic tx :{messageFromFlutter.text} </div> */}
      
        
   </div>
+
+  <div className="flex-col justify-center align-center pt-3">
+        <input
+          id="matic"
+          type="text"
+          placeholder={messageFromFlutter ? messageFromFlutter.text : maticValue}
+          onChange={(e) => setMaticValue(e.target.value)}
+          className='w-40 rounded-3xl text-center text-[#223A7E] pt-1 items-center justify-center text-black text-xl bg-[#DCE5FF]'
+          
+        />
+        MATICS
+        <button
+          onClick={() => {
+        
+
+          }}
+          className={`bg-[#A5BCFF] rounded-[17px] `}>
+          <div className={`bg-[#3166FF] rounded-[15px] pt-2 pb-1 px-3 text-[#FFFFFF] text-md font-octarinebold cursor-pointer`}>
+            submit
+          </div>
+        </button>
+
+        
+
+    {/* <div>Message from Flutter: {messageFromFlutter.text}</div>
+    <div className="text-md">Matic tx :{messageFromFlutter.text} </div> */}
+     
+       
+  </div>
+
     </>
   );
 }
